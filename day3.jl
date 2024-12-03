0b9ea5ab2a2ce556fa5c56793ddd97bcfda262e5
codeo <13805417+codeoxvf@users.noreply.github.com>
2024-12-03 22:11:08 +0800

    day 3 part 2

--- day3.jl
+++ day3.jl
@@ -1,6 +1,6 @@
 lines = ["xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"]
 lines = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"]
-#lines = readlines("input3")
+lines = readlines("input3")
 
 function part1(lines)
   sum = 0
@@ -15,24 +15,24 @@ function part1(lines)
 end
 
 function part2(lines)
-  input = join(lines)
   sum = 0
+  enabled = true
 
-  dont = findall("don't()", input)
-  dontindex = [i[end] for i in dont]
-  n = length(dontindex)
-  doindex = Vector{Int}(undef, n)
-  for i in 1:n
-    doindex[i] = findnext("do()", input, dontindex[i])[end]
+  for m in eachmatch(r"(mul\((\d+),(\d+)\)|don't\(\)|do\(\))",
+    join(lines))
+    RegexMatch
+    if enabled && startswith(m.match, "mul")
+      x = parse(Int, m.captures[2])
+      y = parse(Int, m.captures[3])
+      sum += x * y
+    elseif m.match == "don't()"
+      enabled = false
+    elseif m.match == "do()"
+      enabled = true
+    end
   end
 
-  #=for m in eachmatch(r"mul\((\d+),(\d+)\)", join(lines))
-    x = parse(Int, m.captures[1])
-    y = parse(Int, m.captures[2])
-    sum += x * y
-  end
-
-  return sum=#
+  return sum
 end
 
 println(part2(lines))
