be65661ab5e580312978e058c24e1f694bb841b3
codeo <3805417+codeoxvf@users.noreply.github.com>
2025-01-12 13:17:07 +0800

    day 12 part 2

--- day12.jl
+++ day12.jl
@@ -1,7 +1,28 @@
-input = split("""AAAA
+example1 = split("""AAAA
 BBCD
 BBCC
 EEEC""", '\n', keepempty=false)
+example2 = split("""EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE""", "\n")
+example3 = split("""AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA""", "\n")
+example4 = split("""RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE""", "\n")
 input = readlines("input12")
 
 function part1(input)
@@ -35,33 +56,63 @@ function part1(input)
 end
 
 function part2(input)
-  function sides!(grid, pos, checked)
-    checked[pos...] = true
-    local total = 0
-    foreach([(-1, 0), (1, 0), (0, -1), (0, 1)]) do d
-      next = d .+ pos
-      if !checkbounds(Bool, grid, next...)
-        total += 1
-      elseif grid[next...] != grid[pos...]
-        total += 1
+  function getcornermap(grid)
+    cornermap = zeros(Int, size(grid))
+    for pos in Tuple.(eachindex(IndexCartesian(), grid))
+      cornermap[pos...] += ncorners(grid, pos)
+    end
+    return cornermap
+  end
+
+  ncorners(grid, pos) =
+    sum([(-1, -1), (1, -1), (-1, 1), (1, 1)]) do d
+      adj = [(pos[1] + d[1], pos[2]), (pos[1], pos[2] + d[2])]
+      inbounds = map(x -> checkbounds(Bool, grid, x...), adj)
+      iszero(inbounds) && return 1
+      if all(inbounds)
+        adjvalues = map(x -> grid[x...], adj)
+        all(adjvalues .== grid[pos...]) &&
+          grid[pos .+ d...] != grid[pos...] &&
+          return 1
+        all(adjvalues .!= grid[pos...]) && return 1
       else
-        checked[next...] && return
-        total += sides!(grid, next, checked)
+        return grid[pos...] != grid[adj[findfirst(inbounds)]...]
       end
     end
-    return total
+
+  function getregion!(grid, pos, visited)
+    buffer = copy(visited)
+    visited[pos...] = true
+
+    adj = filter(getadjacent(grid, pos)) do a
+      checkbounds(Bool, grid, a...) &&
+        !visited[a...] &&
+        grid[a...] == grid[pos...]
+    end
+    foreach(adj) do neighbour
+      visited .|= getregion!(grid, neighbour, visited)
+    end
+    return visited .⊻ buffer
   end
 
-  input = permutedims(reduce(hcat, collect.(input)))
-  checked = zeros(Bool, size(input))
-  total = 0
-  for i in eachindex(IndexCartesian(), input)
-    checked[i] && continue
-    c = count(checked)
-    p = sides!(input, Tuple(i), checked)
-    total += (count(checked) - c)p
+  getadjacent(grid, pos) =
+    filter(p -> checkbounds(Bool, grid, p...),
+      map(d -> pos .+ d,
+        [(-1, 0), (1, 0), (0, -1), (0, 1)]))
+
+  grid = permutedims(reduce(hcat, collect.(input)))
+  visited = zeros(Bool, size(grid))
+  cornermap = getcornermap(grid)
+  totalprice = 0
+  while !all(visited)
+    region = getregion!(grid,
+      Tuple(findfirst(.!visited)),
+      visited)
+    area = count(region)
+    ncorners = sum(cornermap .* region)
+    totalprice += area * ncorners
   end
-  return total
+  return totalprice
 end
 
-println(part2(input))
+part2(input)
